52f416198c7f278cf8bc96f540e4e6b9cfafe5fb
Victor Kukshiev <andrey0bolkonsky@gmail.com>
2023-03-16 10:48:33 +0300

    upload template

---
+++ src/main/java/ru/lorquotes/LorquotesApplication.java
@@ -0,0 +1,13 @@
+package ru.lorquotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LorquotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LorquotesApplication.class, args);
+	}
+
+}
